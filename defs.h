408be25d43f508389da8f010a75198f20f0c2210
khoa vo <kvo018@sledge.cs.ucr.edu>
2020-03-09 16:51:29 -0700

    add ktinfo

--- defs.h
+++ defs.h
@@ -121,6 +121,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int             clone(void(*)(),void*);
+void            ktinfo(void);
 
 // swtch.S
 void            swtch(struct context**, struct context*);

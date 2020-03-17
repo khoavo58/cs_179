f9e8e7e9526b82f9af9a339410a05ed1bd741f19
qwertyu098 <60948118+qwertyu098@users.noreply.github.com>
2020-03-17 14:23:59 -0700

    Update proc.c

--- proc.c
+++ proc.c
@@ -610,3 +610,48 @@ void ktinfo(void){
   cprintf("Thread count is:", p->thread_count, "\n");
   cprintf("Current PID:", p->pid, "\n");
 }
+int join(void)
+{
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->pgdir != curproc->pgdir)
+        //looking for procs that have matching pgdir
+        continue;
+      if(p->state != ZOMBIE)
+        //only looking for zombies
+        continue;  
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        pid = p->pid;
+        //add p->tid
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
